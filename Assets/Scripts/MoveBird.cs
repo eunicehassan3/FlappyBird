d6e9d0269a52372f3138df69c8ba65668770e64c
eunicehassan3 <euniceh.info@gmail.com>
2025-02-13 08:50:48 -0600

    made bird flap

--- Assets/Scripts/MoveBird.cs
+++ Assets/Scripts/MoveBird.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float velocity = 3.0f;
     public Rigidbody2D bird; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bird = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            // transform.position = new Vector3(transform.position.x, transform.position.y + 1.0f);
-            bird.AddForce(Vector3.up);
-            // transform.position += Vector3.up * speed * Time.deltaTime;
-            
+            // bird.linearVelocity = Vector2.zero;
+ 
+            // bird.AddForce(Vector2.up * 30f) ;
+            bird.linearVelocity = Vector2.up * velocity;
+    
         }
     }
 }
