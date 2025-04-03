fc991aaad51ec7e7cd6252c457dcf2f32551adfa
eunicehassan3 <euniceh.info@gmail.com>
2025-04-02 20:21:15 -0500

    adjusted the global light
    created a night scene after score hits 1000

--- Assets/Scripts/LightingControlller.cs
+++ Assets/Scripts/LightingControlller.cs
@@ -1,14 +1,18 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class LightingControlller : MonoBehaviour
 {
     public GameObject birdSpotLight;
+    public GameObject backgroundImage;
     private int score; 
+    private Light2D mainLight;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        mainLight = gameObject.GetComponent<Light2D>();
         score = NewMonoBehaviourScript.score;
     }
 
@@ -17,13 +21,18 @@ public class LightingControlller : MonoBehaviour
     {
         score = NewMonoBehaviourScript.score;
         if(score > 1000 && score <= 2000){
-            Light2D mainLight = gameObject.GetComponent<Light2D>();
-            // mainLight.color = new Color(0,0,0);
+            if(mainLight != null){
+                mainLight.intensity = Mathf.Lerp(mainLight.intensity, 0.2f, Time.deltaTime * 2);
+            }
+            
+            backgroundImage.SetActive(false);
             birdSpotLight.SetActive(true);
         }
 
         else{
             birdSpotLight.SetActive(false);
+            backgroundImage.SetActive(true);
+            mainLight.intensity = 1;
         }
     }
 }
